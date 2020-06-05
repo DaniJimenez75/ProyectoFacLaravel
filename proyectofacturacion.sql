627b915a175b041e38d062d7dd2f1f1325aa87fd
Daniel Jiménez <djimenez000000@gmail.com>
2020-06-06 00:45:29 +0200

    Base de datos backup

--- proyectofacturacion.sql
+++ proyectofacturacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-06-2020 a las 11:27:58
+-- Tiempo de generación: 06-06-2020 a las 00:45:08
 -- Versión del servidor: 10.4.10-MariaDB
 -- Versión de PHP: 7.3.12
 
@@ -29,12 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `albaranes` (
-  `idAlbaran` int(11) NOT NULL,
-  `fechaEntrega` date NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `descripcion` varchar(200) NOT NULL,
-  `idProveedor` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -43,25 +41,17 @@ CREATE TABLE `albaranes` (
 --
 
 CREATE TABLE `clientes` (
-  `idCliente` int(11) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellidos` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `ciudad` varchar(30) NOT NULL,
-  `password` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`idCliente`, `username`, `nombre`, `apellidos`, `email`, `telefono`, `ciudad`, `password`) VALUES
-(2, 'alejandro', 'alejandro', 'largo', 'largo@largo.com', '123456789', 'Tarragona', '123'),
-(7, 'dani', '', 'asdsa', 'dani@dani.com', '543216789', 'Tarragona', '202cb962ac59075b'),
-(8, 'dani', '', 'gfdsgfdsgf', 'dani@dani.com', '543216789', 'Tarragona', '41367da481cbf348'),
-(9, 'dani', '', 'gang', 'dani@dani.com', '543216789', 'Tarragona', 'c39bb6048358ecbd');
+  `idCliente` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ciudad` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -70,33 +60,13 @@ INSERT INTO `clientes` (`idCliente`, `username`, `nombre`, `apellidos`, `email`,
 --
 
 CREATE TABLE `facturas` (
-  `idFactura` int(11) NOT NULL,
+  `idFactura` bigint(20) UNSIGNED NOT NULL,
   `fecha` date NOT NULL,
-  `descripción` varchar(150) NOT NULL,
+  `descripción` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `precio` double NOT NULL,
-  `idCliente` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `facturas`
---
-
-INSERT INTO `facturas` (`idFactura`, `fecha`, `descripción`, `precio`, `idCliente`) VALUES
-(1, '2020-05-22', 'fdsafdadas', 12, 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `idCliente` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -116,8 +86,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2019_08_19_000000_create_failed_jobs_table', 1);
+(1, '2020_06_05_091951_create_trabajadores_table', 1),
+(2, '2020_06_05_092121_create_proveedores_table', 1),
+(3, '2020_06_05_092201_create_clientes_table', 1),
+(4, '2020_06_05_092304_create_albaranes_table', 1);
 
 -- --------------------------------------------------------
 
@@ -126,10 +98,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `proveedores` (
-  `idProveedor` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `descripcion` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `idProveedor` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -138,37 +112,22 @@ CREATE TABLE `proveedores` (
 --
 
 CREATE TABLE `trabajadores` (
-  `idTrabajador` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `ciudad` varchar(30) NOT NULL,
-  `password` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ciudad` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `trabajadores`
 --
 
-INSERT INTO `trabajadores` (`idTrabajador`, `username`, `email`, `telefono`, `ciudad`, `password`) VALUES
-(1, 'daniel', 'dani@dani.com', '627888108', 'Tarragona', '123');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `trabajadores` (`id`, `username`, `email`, `telefono`, `ciudad`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'daniel', 'danieñ@daniel.com', '123456789', 'tarragona', '123', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -178,8 +137,7 @@ CREATE TABLE `users` (
 -- Indices de la tabla `albaranes`
 --
 ALTER TABLE `albaranes`
-  ADD PRIMARY KEY (`idAlbaran`),
-  ADD KEY `idProveedor` (`idProveedor`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `clientes`
@@ -192,13 +150,7 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `facturas`
   ADD PRIMARY KEY (`idFactura`),
-  ADD KEY `idCliente` (`idCliente`);
-
---
--- Indices de la tabla `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `facturas_idcliente_foreign` (`idCliente`);
 
 --
 -- Indices de la tabla `migrations`
@@ -216,14 +168,7 @@ ALTER TABLE `proveedores`
 -- Indices de la tabla `trabajadores`
 --
 ALTER TABLE `trabajadores`
-  ADD PRIMARY KEY (`idTrabajador`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -233,65 +178,47 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `albaranes`
 --
 ALTER TABLE `albaranes`
-  MODIFY `idAlbaran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCliente` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `facturas`
 --
 ALTER TABLE `facturas`
-  MODIFY `idFactura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idFactura` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProveedor` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajadores`
 --
 ALTER TABLE `trabajadores`
-  MODIFY `idTrabajador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `albaranes`
---
-ALTER TABLE `albaranes`
-  ADD CONSTRAINT `albaranes_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedores` (`idProveedor`);
-
---
 -- Filtros para la tabla `facturas`
 --
 ALTER TABLE `facturas`
-  ADD CONSTRAINT `facturas_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`);
+  ADD CONSTRAINT `facturas_idcliente_foreign` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
